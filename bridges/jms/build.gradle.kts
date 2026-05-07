7dbeb0ad5bf657515110438d0f3bb29c54c9669a
kennycallado <kennycallado@hotmail.com>
2026-05-07 20:18:28 +0200

    release: v0.8.1
    Version bump only release.
    
    Includes changes since v0.8.0:
    - feat(bean): add WASM bean plugin support
    - feat(bean): implement real WASM bean invocation with wasmtime
    - chore(scripts): remove camel-wasm-sdk from publish script
    
    Release updates:
    - workspace + pinned crate versions -> 0.8.1
    - regenerate Cargo.lock
    - update README pre-release version
    - update JMS bridge default version
    - update isolated example versions

--- bridges/jms/build.gradle.kts
+++ bridges/jms/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.diffplug.spotless") version "6.25.0"
 }
 
-version = project.findProperty("version")?.toString() ?: "0.2.1"
+version = project.findProperty("version")?.toString() ?: "0.8.1"
 
 repositories {
     mavenCentral()
